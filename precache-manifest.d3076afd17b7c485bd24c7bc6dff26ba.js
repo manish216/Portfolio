4671053b3ccc442b2cbcff855191dd46c3702b8a
manish reddy <pai international@FACELESS.MANISH>
2020-07-03 15:01:24 -0400

    Updates

---
+++ precache-manifest.d3076afd17b7c485bd24c7bc6dff26ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166df1105180ac7bbdeca9617f6e7b32",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a090a927da7048b94757",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "10b8bb712f892b159ee0",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a090a927da7048b94757",
+    "url": "/Portfolio/static/js/2.5e796f1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.5e796f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b8bb712f892b159ee0",
+    "url": "/Portfolio/static/js/main.43934adc.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
