860639fa35bc121436ec4956f8663717643c8655
manish216 <manishreddy216@gmail.com>
2020-08-12 23:19:45 -0400

    typeText intial Trail

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
+import styles from  './home.module.css';
+import { init } from 'ityped';
+
+
+
+
 class Home extends Component {
+    componentDidMount() {
+        const myElement = document.querySelector('#myElement')
+        init(myElement, {   strings: ['Software Developer!','Data Engineer!','Data Scientist!','Machine Learning Engineer!'],
+                            typeSpeed:100,
+                            backSpeed:50,
+                            startDelay:500,
+                            backDelay:500,
+                            loop: true,
+                            showCursor:true,
+                            cursorChar: "|",
+                        }
+            )
+    }
     render() {
         return (
-            <div>
-                <h1>Home</h1>
+            <div className ={styles.pageWrapper}>
+                <div id="myElement"></div>
+
+                {/* <div className={styles.pageBackground}>
+                </div> */}
             </div>
         );
     }
